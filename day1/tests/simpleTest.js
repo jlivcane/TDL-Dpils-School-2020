5797b341f119566bd0405491a50b0657f4d3400f
Jekaterina Livcane <siatestdevlab@school.local>
2020-09-10 17:29:24 +0300

    day1

---
+++ day1/tests/simpleTest.js
@@ -0,0 +1,12 @@
+module.exports = {
+	function(client) {
+		client
+			.url('https://www.google.com')
+			.pause(5 * 1000)
+			.element('css selector', 'iframe[src]', result => {
+				client.frame(result.value).click('#introAgreeButton');
+			})
+			.setValue('input[title="Search"]', ['TestDevLab', client.Keys.ENTER])
+			.pause(5 * 1000);
+	}
+};
