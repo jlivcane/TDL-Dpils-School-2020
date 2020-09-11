40f3d3cc39471ef40b839b851d2dcc61278a96dc
Jekaterina Livcane <siatestdevlab@school.local>
2020-09-11 10:21:39 +0300

    day2

--- day2/tests/file_io_test.js
+++ day2/tests/file_io_test.js
@@ -1,5 +1,10 @@
 module.exports = {
-    function(client) {
-        client.url('https://wwww.file.io/').pause(5 * 1000);
-    }
-}
+	function(client) {
+		client
+			.url('https://www.file.io/')
+			//.pause(5 * 1000)
+			.waitForElementVisible('body', 5 * 1000)
+			.waitForElementPresent('input[type="file"]', 5 * 1000)
+			.setValue('input[type="file"]', require('path').resolve('./assets/panda.jpg'));
+	}
+};
